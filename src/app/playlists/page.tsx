ad85658f051611ee11957b98930ae3cbdd52f725
Manjunath Irukulla <imanjunad@gmail.com>
2025-04-22 16:26:29 +0530

    Playlist 1.0.0

--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -2,6 +2,7 @@ import { AppSidebar } from "@/components/app-sidebar"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import { SiteHeader } from "@/components/site-header"
 import { InfiniteSlider } from "components/motion-primitives/infinite-slider"
+import ExpandableCardDemo from "@/components/ui/expandablecard"
 const Playlists = ()=> {
   return (
     <SidebarProvider>
@@ -13,7 +14,7 @@ const Playlists = ()=> {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <h1 className="px-4 text-2xl font-bold lg:px-6">Playlists</h1>
               {/* Analytics content goes here */}
-    
+              <ExpandableCardDemo />
             </div>
           </div>
         </div>
